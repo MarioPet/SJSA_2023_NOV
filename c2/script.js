666d7b201030958aed975c2348ba03c19e1ef49d
Mario Petkovski <mario5et@Marios-MacBook-Pro-2.local>
2023-05-04 21:47:16 +0200

    Variables

---
+++ c2/script.js
@@ -0,0 +1,72 @@
+// Variables
+
+// right variable name;
+// var _name = 'Mario';
+
+// wrong variable name
+// var e-mail = 'mario5etkovski@gmail.com';
+
+// right variable name
+// var e_mail = 'mario5etkovski@gmail.com';
+
+// wrong variable name
+// var e mail = 'mario5etkovski@gmail.com';
+
+// right variable name
+// var firstName = 'Mario';
+// var LastName = 'Petkovski';
+
+// var ___eMail;
+// ___eMail = 'pero.perovski@gmail.com';
+
+// _name = 'Martin';
+
+// console.log(_name);
+
+// var num = 42;
+
+// var num_a = 5;
+// var num_b = 5;
+
+// var result = num_a - num_b;
+
+// console.log(result);
+
+// var prompt_result = prompt("Enter your name");
+// console.log(prompt_result);
+
+
+// 4 prompts
+// 1. Enter your name
+// 2. Enter your e-mail
+// 3. Enter the language you're currently learning
+// 4. Enter the academy where you are learning this language
+
+// var ask_name = prompt("Please enter your name");
+// var ask_mail = prompt("Please enter your e-mail address");
+// var ask_prog_lang = prompt("What programming language are you currently learning?");
+// var ask_academy = prompt("In which academy are you learning this language?");
+
+// console.log('Your name is ' + ask_name + '. Your e-mail is ' + ask_mail + '. You\'re studying ' + ask_prog_lang + ' at ' + ask_academy);
+
+// console.log(`Your name is ${ask_name}. Your e-mail is ${ask_mail}. You're studying ${ask_prog_lang} at ${ask_academy}`);
+
+// 'string' + ask_name + 'other string'
+
+
+// ====================================================================================
+
+// Boolean / true / false
+
+
+// console.log(10 > 5);
+// console.log(10 == 5);
+
+var num1 = 30;
+var num2 = 20;
+
+if (num1 > num2) {
+    console.log(`Number ${num1} is larger than number ${num2}.`);
+}
+
+console.log(10 / 2 * 12);
